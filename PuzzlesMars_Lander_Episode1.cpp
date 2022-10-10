2a29392aa00ba87fdb4428912534736ae5c92294
MartynasBudrevicius <115450331+MartynasBudrevicius@users.noreply.github.com>
2022-10-10 22:11:38 +0300

    Add files via upload

---
+++ PuzzlesMars_Lander_Episode1.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int surfaceN, temp;
+    cin >> surfaceN; cin.ignore();
+    for(int i = 0; i < surfaceN; i++){
+        cin >> temp >> temp; cin.ignore();
+    }
+    while(1){
+        int vSpeed;
+        cin >> temp >> temp >> temp >> vSpeed >> temp >> temp >> temp; cin.ignore();
+        cout << 0 << " ";
+        if(abs(vSpeed) > 35) cout << 4 << endl;
+        else cout << 0 << endl;
+    }
+}
