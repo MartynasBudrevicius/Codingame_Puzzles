a73d3f6ae4f7127c7390a4d09cf4772d7b6b11a7
Martynas Budrevičius <115450331+MartynasBudrevicius@users.noreply.github.com>
2022-11-10 22:21:05 +0200

    Create Carmichael_numbers.cpp

---
+++ Medium/Carmichael_numbers.cpp
@@ -0,0 +1,30 @@
+//https://www.codingame.com/training/medium/carmichael-numbers
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n
+    bool isPrime = true;
+    for(int i = 2; i <= n / 2; i++){
+        if(n % i == 0){
+            isPrime = false;
+            break;
+        }
+    }
+    bool good = false;
+    int number = 2;
+    for(int i = 2; i <= n; i++){
+        number *= 2;
+        if(number > n)
+            number -= n;
+    }
+    if(number % n == 2)
+        good = true;   
+    if(!isPrime && good){
+        cout << "YES" << endl;
+        return 0;
+    }
+    cout << "NO" << endl;
+}
