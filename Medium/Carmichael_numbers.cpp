aaaf887c4216dd7c9b2fb92b51366a1d822ab716
Martynas Budrevičius <115450331+MartynasBudrevicius@users.noreply.github.com>
2022-11-10 22:21:28 +0200

    Update Carmichael_numbers.cpp

--- Medium/Carmichael_numbers.cpp
+++ Medium/Carmichael_numbers.cpp
@@ -1,11 +1,10 @@
-//https://www.codingame.com/training/medium/carmichael-numbers
 #include <bits/stdc++.h>
 
 using namespace std;
 
 int main(){
     int n;
-    cin >> n
+    cin >> n;
     bool isPrime = true;
     for(int i = 2; i <= n / 2; i++){
         if(n % i == 0){
